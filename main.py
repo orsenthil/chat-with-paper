c89cb9c27d3cb96e85a83d178b4177cd3a9de19e
Senthil Kumaran <senthil@python.org>
2024-06-10 05:25:24 -0700

    Updated main and requirements.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import os
 import tempfile
 from enum import Enum
+from typing import Dict, Any, Optional, List
+from uuid import UUID
 
 import streamlit as st
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import EmbeddingsFilter
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import DocArrayInMemorySearch
+from langchain_core.callbacks import BaseCallbackHandler
+from langchain_openai import ChatOpenAI
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -14,8 +22,52 @@ GPT3_LLM_MODEL = "gpt-3.5-turbo"
 GPT4_LLM_MODEL = "gpt-4"
 GPT4o_LLM_MODEL = "gpt-4-o"
 
+uploaded_files = []
+chain = None
 
-def get_document_text(files, use_compression=False):
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text: str=""):
+        self.container = container
+        self.text = initial_text
+        self.run_id_ignore_token = None
+
+    def on_llm_start(self, serialized:dict, prompts: list, **kwargs):
+        self.container.write(f"LLM Start: {serialized}")
+        if prompts[0].startswith("Human"):
+            self.run_id_ignore_token = kwargs.get("run_id")
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        if self.run_id_ignore_token == kwargs.get("run_id", False):
+            return
+        self.text += token
+        self.container.markdown(self.text)
+
+
+class PrintRetrievalHandler(BaseCallbackHandler):
+    def __init__(self, container):
+        self.status = container.status("thinking...")
+        self.container = container
+
+    def on_retriever_start(
+        self,
+        serialized: Dict[str, Any],
+        query: str,
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        tags: Optional[List[str]] = None,
+        metadata: Optional[Dict[str, Any]] = None,
+        **kwargs: Any,
+    ) -> Any:
+        self.status.text("Retrieving...")
+
+    def on_retriever_end(self, documents, **kwargs):
+        self.status.text("Retrieved.")
+        self.container.empty()
+
+
+def document_retriever(files, use_compression=False):
     docs = []
     temp_dir = tempfile.TemporaryDirectory()
     for file in files:
@@ -40,7 +92,23 @@ def get_document_text(files, use_compression=False):
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     vectordb = DocArrayInMemorySearch.from_documents(splits, embeddings)
 
+    # Define retriever
+    retriever = vectordb.as_retriever(
+        search_type="mmr", search_kwargs={"k": 2, "fetch_k": 4}
+    )
 
+    if not use_compression:
+        return retriever
+
+    compressor = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=0.76)
+
+    return ContextualCompressionRetriever(base_compressor=compressor, base_retriever=retriever)
+
+
+
+class ChatProfileRoleEnum(str, Enum):
+    HUMAN = "Human"
+    AI = "AI"
 
 
 class LLMProviderEnum(str, Enum):
@@ -178,10 +246,49 @@ with st.sidebar:
                                               type=["pdf", "txt"],
                                               accept_multiple_files=True,
                                               disabled=(not selected_model or not api_key))
-        if not selected_model:
-            st.info("Please select a model to chat with.")
-        if not api_key:
-            st.info(f"Please enter the API key of {selected_model} to chat with the paper.")
+if not selected_model:
+    st.info("Please select a model to chat with.")
+if not api_key:
+    st.info(f"Please enter the API key of {selected_model} to chat with the paper.")
 
+if uploaded_files:
+    retriever = document_retriever(uploaded_files, use_compression=False)
+    if retriever is not None:
+        st.write("Retriever created successfully.")
 
+    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=messages, return_messages=True)
+    if selected_model in [LLMProviderEnum.GPT3, LLMProviderEnum.GPT4, LLMProviderEnum.GPT4o]:
+        st.write(f"Chatting with {LLMProviderEnum(selected_model).value} model.")
+        llm = ChatOpenAI(model=model_name, api_key=api_key, temperature=0, streaming=True)
 
+        if llm is None:
+            st.error("Failed to create the LLM model. Please check your API Key.")
+
+        # Create the Conversational RetrievalChain using the LLM instance.
+        chain = ConversationalRetrievalChain.from_llm(
+            llm=llm,
+            retriever=retriever,
+            memory=memory,
+            verbose=True,
+            max_tokens_limit=4000,
+        )
+
+        avatars = {
+            ChatProfileRoleEnum.AI.value: "AI",
+            ChatProfileRoleEnum.HUMAN.value: "Human",
+        }
+
+        for msg in messages.messages:
+            st.chat_message(avatars[msg.type]).write(msg.content)
+
+# Get User Input and Generate Response
+
+if user_query := st.chat_input(placeholder="Ask me Anything!", disabled=(not uploaded_files)):
+    st.chat_message("Human").write(user_query)
+    with st.chat_message("AI"):
+        retrieval_handler = PrintRetrievalHandler(st.empty())
+        stream_handler = StreamHandler(st.empty())
+        response = chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
+
+if selected_model and model_name:
+    st.sidebar.caption(f"Using {selected_model} model.")
