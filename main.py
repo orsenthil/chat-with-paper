1947c8d01490232865a3d00a5c1465c1ceef4af8
Senthil Kumaran <senthil@python.org>
2024-06-15 12:16:37 -0700

    Removed Extra Models.

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ from langchain_openai import ChatOpenAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 GPT3_LLM_MODEL = "gpt-3.5-turbo"
-GPT4_LLM_MODEL = "gpt-4"
-GPT4o_LLM_MODEL = "gpt-4-o"
+GPT4_LLM_MODEL = "gpt-4-turbo"
+GPT4o_LLM_MODEL = "gpt-4o"
 
 uploaded_files = []
 chain = None
@@ -113,37 +113,6 @@ class LLMProviderEnum(str, Enum):
     GPT3 = "GPT-3"
     GPT4 = "GPT-4"
     GPT4o = "GPT-4o"
-    BERT = "BERT"
-    RoBERTa = "RoBERTa"
-    DistilBERT = "DistilBERT"
-    T5 = "T5"
-    XLNet = "XLNet"
-    ALBERT = "ALBERT"
-    Electra = "Electra"
-    BART = "BART"
-    CamemBERT = "CamemBERT"
-    Pegasus = "Pegasus"
-    ProphetNet = "ProphetNet"
-    MBart = "MBart"
-    MarianMT = "MarianMT"
-    XLM = "XLM"
-    XLM_R = "XLM-R"
-    TuringNLG = "Turing-NLG"
-    DialoGPT = "DialoGPT"
-    BlenderBot = "BlenderBot"
-    Meena = "Meena"
-    LaMDA = "LaMDA"
-    DALLE = "DALL-E"
-    CLIP = "CLIP"
-    VQ_VAE_2 = "VQ-VAE-2"
-    StyleGAN = "StyleGAN"
-    BigGAN = "BigGAN"
-    StyleGAN2 = "StyleGAN2"
-    StyleGAN3 = "StyleGAN3"
-    BigGAN_512px = "BigGAN-512px"
-    StyleGAN2_ADA = "StyleGAN2-ADA"
-    StyleGAN2_ADA_512px = "StyleGAN2-ADA-512px"
-    StyleGAN2_ADA_1024px = "StyleGAN2-ADA-1024px"
 
 
 # Setup the sidebar for streamlit
@@ -175,37 +144,6 @@ with st.sidebar:
                 LLMProviderEnum.GPT3.value,
                 LLMProviderEnum.GPT4.value,
                 LLMProviderEnum.GPT4o.value,
-                LLMProviderEnum.BERT.value,
-                LLMProviderEnum.RoBERTa.value,
-                LLMProviderEnum.DistilBERT.value,
-                LLMProviderEnum.T5.value,
-                LLMProviderEnum.XLNet.value,
-                LLMProviderEnum.ALBERT.value,
-                LLMProviderEnum.Electra.value,
-                LLMProviderEnum.BART.value,
-                LLMProviderEnum.CamemBERT.value,
-                LLMProviderEnum.Pegasus.value,
-                LLMProviderEnum.ProphetNet.value,
-                LLMProviderEnum.MBart.value,
-                LLMProviderEnum.MarianMT.value,
-                LLMProviderEnum.XLM.value,
-                LLMProviderEnum.XLM_R.value,
-                LLMProviderEnum.TuringNLG.value,
-                LLMProviderEnum.DialoGPT.value,
-                LLMProviderEnum.BlenderBot.value,
-                LLMProviderEnum.Meena.value,
-                LLMProviderEnum.LaMDA.value,
-                LLMProviderEnum.DALLE.value,
-                LLMProviderEnum.CLIP.value,
-                LLMProviderEnum.VQ_VAE_2,
-                LLMProviderEnum.StyleGAN,
-                LLMProviderEnum.BigGAN,
-                LLMProviderEnum.StyleGAN2,
-                LLMProviderEnum.StyleGAN3,
-                LLMProviderEnum.BigGAN_512px,
-                LLMProviderEnum.StyleGAN2_ADA,
-                LLMProviderEnum.StyleGAN2_ADA_512px,
-                LLMProviderEnum.StyleGAN2_ADA_1024px
             ],
             index=0,
             placeholder="Select a model to analyze the paper with."
